c8dd81e34ade316e4491202561330cc845ada552
emerix1993 <emerix@ukr.net>
2023-05-01 03:10:13 +0300

    with edits

--- Homework_7/homework_currency.py
+++ Homework_7/homework_currency.py
@@ -50,17 +50,6 @@ class Menu:
             print(f"commision system is -> {commision_operation} GBP")
             return f"you total is: {converted_amount - commision_operation}"
 
-    # def convert_from(self, currency: str, amount: float) -> 'Price':
-    #     if self.currency == currency:
-    #         return Price(self.amount, self.currency)
-    #     else:
-    #         # convert to USD first
-    #         usd_amount = self.convert_to('USD').amount
-    #         # convert to target currency
-    #         converted_amount = Price(usd_amount, 'USD').convert_to(currency).amount
-    #         return Price(converted_amount, currency)
-
-
 def main():
     while True:
         type_currency = input("Enter you currency: ")
@@ -79,67 +68,38 @@ def main():
             pass
         action = input("Change you action: ")
         if action == "convert":
-            pass
-        type_exchange_currency = input("Enter currency: ")
-        if type_exchange_currency == type_currency:
-            print("This is same currencies: ")
-        elif type_exchange_currency == "USD":
-            menu = Menu(amount,type_currency)
-            print(menu.another_currency_to_usd(amount, type_currency))
-        elif type_exchange_currency == "EUR":
-            menu = Menu(amount,type_currency)
-            print(menu.another_currency_to_eur(amount, type_currency))
-        elif type_exchange_currency == "UAH":
-            menu = Menu(amount, type_currency)
-            print(menu.another_currency_to_uah(amount, type_currency))
-        elif type_exchange_currency == "GBP":
-            menu = Menu(amount, type_currency)
-            print(menu.another_currency_to_gbp(amount, type_currency))
+            type_exchange_currency = input("Enter currency: ")
+            if type_exchange_currency == type_currency:
+                print("This is same currencies: ")
+            elif type_exchange_currency == "USD":
+                menu = Menu(amount,type_currency)
+                print(menu.another_currency_to_usd(amount, type_currency))
+            elif type_exchange_currency == "EUR":
+                menu = Menu(amount,type_currency)
+                print(menu.another_currency_to_eur(amount, type_currency))
+            elif type_exchange_currency == "UAH":
+                menu = Menu(amount, type_currency)
+                print(menu.another_currency_to_uah(amount, type_currency))
+            elif type_exchange_currency == "GBP":
+                menu = Menu(amount, type_currency)
+                print(menu.another_currency_to_gbp(amount, type_currency))
+        elif action == "add":
+            add_currency = input("Enter add currency: ")
+            if type_currency == add_currency:
+                amount_add = int(input("Enter add amount: "))
+                print(f"result: {amount + amount_add}")
+            else:
+                print("For this operation you need same currencies")
+        elif action == "sub":
+            sub_currency = input("Enter add currency: ")
+            if type_currency == sub_currency:
+                amount_add = int(input("Enter add amount: "))
+                print(f"result: {amount - amount_add}")
+            else:
+                print("For this operation you need same currencies")
+
+
 
 
 if __name__ == "__main__":
     main()
-
-#
-#     def __add__(self, other):
-#         if self.currency == other.currency:
-#             result_amount = self.amount + other.amount
-#             result_currency = self.currency
-#         else:
-#             converted_amount = self.convert_to('USD').amount + other.convert_to('USD').amount
-#             result_amount = self.convert_from('USD', converted_amount).amount
-#             result_currency = self.currency if self.currency != 'USD' else other.currency
-#         return Price(result_amount, result_currency)
-#
-#     def __sub__(self, other: "Price") -> 'Price':
-#         if self.currency == other.currency:
-#             result_amount = self.amount - other.amount
-#             result_currency = self.currency
-#         else:
-#             converted_amount = self.convert_to('USD').amount - other.convert_to('USD').amount
-#             result_amount = self.convert_from('USD', converted_amount).amount
-#             result_currency = self.currency if self.currency != 'USD' else other.currency
-#         return Price(result_amount, result_currency)
-#
-#     def convert_to(self, currency: str) -> 'Price':
-#         if self.currency == currency:
-#             return self
-#         else:
-#             converted_amount = self.amount * self.exchange_rates[currency] / self.exchange_rates[self.currency]
-#             return Price(converted_amount, currency)
-#
-#     def convert_from(self, currency: str, amount: float) -> 'Price':
-#         if self.currency == currency:
-#             return Price(amount, self.currency)
-#         else:
-#             converted_amount = amount * self.exchange_rates[self.currency] / self.exchange_rates[currency]
-#             return Price(converted_amount, self.currency)
-#
-#     def __repr__(self) -> str:
-#         return f'{self.amount:.2f} {self.currency}'
-#
-# price1 = Price(100,"USD")
-# price2 = Price(50, 'EUR')
-# price3 = Price(2000,"UAH")
-# result = price1 + price2 + price3
-# print(result) # выведет "141.67 USD"
